f94580a2fead184236efd65de46f50556ee80baf
wluka85 <wluka85@gmail.com>
2018-10-29 14:49:31 +0100

    Add style to navi bar

--- js/view/buttonCreator.js
+++ js/view/buttonCreator.js
@@ -3,6 +3,7 @@ export default class ButtonCreator {
     getButton(name, action) {
         let naviButtonDiv = document.createElement('div');
         naviButtonDiv.innerText = name;
+        naviButtonDiv.setAttribute('class', 'navi-button');
         naviButtonDiv.addEventListener('click', action);
 
         return naviButtonDiv;
