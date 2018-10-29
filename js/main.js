1168cab2dc66d21c2a32c05d0a08096659c2be90
wluka85 <wluka85@gmail.com>
2018-10-29 14:33:51 +0100

    Implement Main class

---
+++ js/main.js
@@ -0,0 +1,14 @@
+import Controller from "./controller/controller.js";
+import View from "./view/view.js";
+import Model from "./model/model.js";
+
+class Main {
+
+    constructor() {
+        this.model = new Model();
+        this.controller = new Controller(this.model);
+        this.view = new View(this.controller);
+    }
+}
+
+const main = new Main();
