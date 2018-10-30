d7c9177083c58daae1c5bdb8fc66161e558bb8b5
Senseinvader <d.ivantsov1983@gmail.com>
2018-10-30 11:37:32 +0100

    Create button creator class for navi menu buttons

--- js/view/buttonCreator.js
+++ js/view/buttonCreator.js
@@ -2,9 +2,9 @@ export default class ButtonCreator {
 
     getButton(name, action) {
         let naviButtonDiv = document.createElement('div');
+        naviButtonDiv.classList.add('nav-element');
         naviButtonDiv.innerText = name;
         naviButtonDiv.addEventListener('click', action);
-
         return naviButtonDiv;
     }
 }
